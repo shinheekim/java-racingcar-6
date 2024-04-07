1a0d5f54c22112b58b920f757aeeb4aa43de92ec
CINI <shinhee1241@gmail.com>
2024-04-07 21:42:48 +0900

    feat: 자동차 게임 간단 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -3,5 +3,12 @@ package racingcar;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+    Car car = new Car();
+    car.printCarName();
+    car.inputCarName();
+    car.printMoveCount();
+    car.playGame(car.getCarName(), car.getCarPositions());
+    car.winnerCar(car.getCarName(),car.getCarPositions());
+    car.printWinnerList(car.getWinnerCars());
     }
 }
